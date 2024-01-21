b01e799b906758d73cc869ebd8fcc37e5c584f46
pavel <p.kruchkovsky@aspex.kz>
2024-01-21 16:08:20 +0500

    version 1.2.8

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-MONGODB_URL = "mongodb+srv://contosomongodb:St6Fd4Bhtj35ib!wu@ax7monogodb.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
-# MONGODB_URL = "mongodb://admin:St6Fd4Bhtj35ib!wu@localhost:27017/"
+#MONGODB_URL = "mongodb+srv://contosomongodb:St6Fd4Bhtj35ib!wu@ax7monogodb.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
+MONGODB_URL = "mongodb://root:St6Fd4Bhtj35ib!wu@0.0.0.0:27017/"
 TELEGRAM_BOT_TOKEN = "6970315166:AAFtD5E78OR-F26zZTpDHxiC2vzno6IfmSs"
 score_start_frt = 1.5
 score_start_second = 3
